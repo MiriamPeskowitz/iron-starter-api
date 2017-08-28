ff29fb008ce26dc89be2e79524f36a51626c1ea0
Lukeghenco <lukeghenco@gmail.com>
2017-08-28 13:00:57 -0400

    Add action and tests for GET /api/campaigns/:campaign_id/comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,19 @@
 class API::CommentsController < ApplicationController
+    before_action :set_campaign 
+    before_action :set_campaign_comment, only: [:show, :update, :destroy]
+
+    # GET /api/campaigns/:campaign_id/comments 
+    def index 
+        json_response(@campaign.comments)
+    end
+
+    private 
+
+        def set_campaign
+            @campaign = Campaign.find(params[:campaign_id])
+        end 
+
+        def set_campaign_comment
+            @comment = @campaign.comments.find_by!(id: params[:id])
+        end
 end
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments API', type: :request do
+    # Initial Test Data
+    let!(:campaign) { create(:campaign) }
+    let!(:comments) { create_list(:comment, 10, campaign_id: campaign.id) }
+    let(:campaign_id) { campaign.id }
+    let(:comment_id) { comments.first.id }
 
+    # Tests for GET /api/campaigns/:campaign_id/comments 
+    describe 'GET /api/campaigns/:campaign_id/comments' do  
+        before { get "/api/campaigns/#{campaign_id}/comments" } 
+
+        context 'when campaign exists' do   
+            it 'returns status code 200' do
+                expect(response).to have_http_status(200)
+            end
+
+            it 'returns all campaign comments' do  
+                expect(json.size).to eq(10) 
+            end 
+        end 
+
+        context 'when campaign does not exist' do  
+            let(:campaign_id) { 0 }
+
+            it 'returns status code 404' do  
+                expect(response).to have_http_status(404) 
+            end 
+
+            it 'returns a not found message' do  
+                expect(response.body).to match(/Couldn't find Campaign with 'id'=0/)
+            end
+        end
+    end
 end
