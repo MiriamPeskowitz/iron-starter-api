3f6dafee5d3eead72a737aa06731ce7f98c42773
Lukeghenco <lukeghenco@gmail.com>
2017-08-28 10:51:33 -0400

    Fix syntax error for Faker time in campaign factory

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
         description { Faker::Lorem.paragraph }
         goal { Faker::Number.between(1000, 1000000) } 
         pledged { Faker::Number.between(0, 10) }
-        deadline { DateTime.now + 15, DateTime.now + 30) }
+        deadline { Faker::Time.between(DateTime.now + 15, DateTime.now + 30) }
     end 
 end
         
