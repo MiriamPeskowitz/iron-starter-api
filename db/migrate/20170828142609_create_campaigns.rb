deb2b3a50673bc089c041c419d88810e5b9c5c63
Lukeghenco <lukeghenco@gmail.com>
2017-08-28 10:28:37 -0400

    Add Comment model

--- db/migrate/20170828142609_create_campaigns.rb
+++ db/migrate/20170828142609_create_campaigns.rb
@@ -2,7 +2,7 @@ class CreateCampaigns < ActiveRecord::Migration[5.1]
   def change
     create_table :campaigns do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.integer :goal
       t.integer :pledged
       t.timestamp :deadline
