7f904a4312ee5189b9ad2ceb86b579e6372e6bfe
Lukeghenco <lukeghenco@gmail.com>
2017-08-28 10:58:15 -0400

    Add action and tests for GET /api/campaigns

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -1,2 +1,9 @@
 class API::CampaignsController < ApplicationController
+
+
+    # GET /api/campaigns 
+    def index 
+        @campaigns = Campaign.all 
+        json_response(@campaigns) 
+    end
 end
--- spec/requests/campaigns_spec.rb
+++ spec/requests/campaigns_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Campaigns API', type: :request do
 
+    let!(:campaigns) { create_list(:campaign, 10) }
+    let(:campaign_id) { campaigns.first.id }
+
+    describe 'GET /api/campaigns' do  
+
+        before { get '/api/campaigns' }
+
+        it 'returns campaigns' do  
+            expect(json).not_to be_empty 
+            expect(json.size).to eq(10)
+        end 
+
+        it 'returns status code 200' do 
+            expect(response).to have_http_status(200)
+        end 
+    end
 end
