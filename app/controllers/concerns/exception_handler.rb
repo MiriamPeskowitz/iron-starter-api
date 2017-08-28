08271ef3b6ed42d13f8eb895d6ec6b5e22290eb4
Lukeghenco <lukeghenco@gmail.com>
2017-08-28 10:57:41 -0400

    Add exception handler for ActiveRecord errors

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,15 @@
+module ExceptionHandler 
+    extend ActiveSupport::Concern 
+
+    included do  
+        rescue_from ActiveRecord::RecordNotFound do |entity| 
+            json_response({ message: entity.message }, :not_found)
+        end 
+
+        rescue_from ActiveRecord::RecordInvalid do |entity| 
+            json_response({ message: entity.message }, :unprocessable_entity)
+        end
+    end 
+
+end
+
