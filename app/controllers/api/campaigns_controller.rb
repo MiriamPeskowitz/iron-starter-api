38d03c803fa8079cda3de098bd5710ad1634edf6
Lukeghenco <lukeghenco@gmail.com>
2017-08-28 11:12:15 -0400

    Add action and tests for GET /api/campaigns/:id

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -1,9 +1,18 @@
 class API::CampaignsController < ApplicationController
-
+    before_action :set_campaign, only: [:show, :update, :destroy] 
 
     # GET /api/campaigns 
     def index 
         @campaigns = Campaign.all 
         json_response(@campaigns) 
     end
+
+    def show 
+        json_response(@campaign)
+    end
+
+    private 
+        def set_campaign  
+            @campaign = Campaign.find(params[:id]) 
+        end
 end
--- spec/requests/campaigns_spec.rb
+++ spec/requests/campaigns_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Campaigns API', type: :request do
-
+    # Initialize Test Data
     let!(:campaigns) { create_list(:campaign, 10) }
     let(:campaign_id) { campaigns.first.id }
 
+    # Tests for GET /api/campaigns
     describe 'GET /api/campaigns' do  
 
         before { get '/api/campaigns' }
@@ -18,4 +19,22 @@ RSpec.describe 'Campaigns API', type: :request do
             expect(response).to have_http_status(200)
         end 
     end
+
+    # Tests for GET /api/campaigns/:id 
+    describe 'GET /api/campaigns/:id' do  
+        before { get "/api/campaigns/#{campaign_id}" }
+
+        context 'when campaign exists' do 
+            it 'returns the campaign' do  
+                expect(json).not_to be_empty 
+                expect(json[:id]).to eq(campaign_id) 
+            end
+
+            it 'returns status code 200' do  
+                expect(response).to have_http_status(200) 
+            end
+        end
+    end
+
+
 end
